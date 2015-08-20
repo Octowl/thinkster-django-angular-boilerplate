2d801c77892ed4a540ebdbaf8f46f5a419e2da0e
Octowl <alsaff1987@gmail.com>
2015-08-20 16:48:58 +0300

    Configure angular CSRF settings

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -13,5 +13,20 @@
 
     angular
         .module('thinkster.config', []);
+
+    angular
+        .module('thinkster')
+        .run(run);
+
+    run.$inject = ['$http'];
+
+    /**
+     * @name run
+     * @desc Update xsrf $http headers to align with Django's defaults
+     */
+    function run($http) {
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCookieName = 'csrftoken';
+    }
 })();
 
