86377352b78f99711510608207c0e0e205e9f305
Octowl <alsaff1987@gmail.com>
2015-08-22 20:29:09 +0300

    Make a PostViewSet viewset

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from posts.models import Post
+from posts.permissions import IsAuthorOfPost
+from posts.serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.order_by('-created_at')
+    serializer_class = PostSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+        return (permissions.IsAuthenticated(), IsAuthorOfPost(),)
+
+    def perform_create(self, serializer):
+        instance = serializer.save(author=self.request.user)
+
+        return super(PostViewSet, self).perform_create(serializer)
+
