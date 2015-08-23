ee97d0b79e7c2af4832fa79ca44dbb2a46cd0c1a
Octowl <alsaff1987@gmail.com>
2015-08-23 13:18:39 +0300

    Add route for '/' index

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -12,14 +12,20 @@
      * @desc Define valid application routes
      */
     function config($routeProvider) {
-        $routeProvider.when('/register',{
-            controller: 'RegisterController',
-            controllerAs: 'vm',
-            templateUrl: '/static/templates/authentication/register.html'
-        }).when('/login', {
-            controller: 'LoginController',
-            controllerAs: 'vm',
-            templateUrl: '/static/templates/authentication/login.html'
-        }).otherwise('/');
+        $routeProvider
+            .when('/', {
+                controller: 'IndexController',
+                controllerAs: 'vm',
+                templateUrl: '/static/templates/layout/index.html'
+            })
+            .when('/register',{
+                controller: 'RegisterController',
+                controllerAs: 'vm',
+                templateUrl: '/static/templates/authentication/register.html'
+            }).when('/login', {
+                controller: 'LoginController',
+                controllerAs: 'vm',
+                templateUrl: '/static/templates/authentication/login.html'
+            }).otherwise('/');
     }
 })();
