14184b75260bffb44b55b96294107812e94c5a43
Octowl <alsaff1987@gmail.com>
2015-08-24 18:02:46 +0300

    Add a route for profile settings

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -22,14 +22,21 @@
                 controller: 'RegisterController',
                 controllerAs: 'vm',
                 templateUrl: '/static/templates/authentication/register.html'
-            }).when('/login', {
+            })
+            .when('/login', {
                 controller: 'LoginController',
                 controllerAs: 'vm',
                 templateUrl: '/static/templates/authentication/login.html'
-            }).when('/+:username', {
+            })
+            .when('/+:username', {
                 controller: 'ProfileController',
                 controllerAs: 'vm',
                 templateUrl: '/static/templates/profiles/profile.html'
+            })
+            .when('/+:username/settings', {
+                controller: 'ProfileSettingsController',
+                controllerAs: 'vm',
+                templateUrl: '/static/templates/profiles/settings.html'
             })
             .otherwise('/');
     }
