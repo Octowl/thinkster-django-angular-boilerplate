d1ebfc80a5ae759176e04abc6e96ed229b5c930c
Octowl <alsaff1987@gmail.com>
2015-08-24 13:45:35 +0300

    Add missing line

--- posts/views.py
+++ posts/views.py
@@ -20,6 +20,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
         return super(PostViewSet, self).perform_create(serializer)
 
+
 class AccountPostsViewSet(viewsets.ViewSet):
     queryset = Post.objects.select_related('author').all()
     serializer_class = PostSerializer
